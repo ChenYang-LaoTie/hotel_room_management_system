d84906fe887f67967255cbf466077b0ad7205c9e
ChenYang <775517776@qq.com>
2020-09-28 09:48:05 +0800

    创建项目完成，git上传完成

---
+++ src/main/java/com/lssf/hotel_room_management_system/HotelRoomManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lssf.hotel_room_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelRoomManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelRoomManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lssf/hotel_room_management_system/HotelRoomManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lssf.hotel_room_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelRoomManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
